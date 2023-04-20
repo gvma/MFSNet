e82e7d4a8fb901f99b2736c7ea8305e3db9558a9
Guilherme <guiganey@gmail.com>
2023-04-20 17:29:29 -0300

    Updates

--- train.py
+++ train.py
@@ -404,37 +404,38 @@ class aggregation(nn.Module):
 
         return x
 
-
 class MFSNet(nn.Module):
     # res2net based encoder decoder
     def __init__(self, channel=32,n_class=1):
         super(MFSNet, self).__init__()
         # ---- ResNet Backbone ----
-        self.resnet = res2net50_v1b_26w_4s(pretrained=False)
+        self.resnet = res2net50_v1b_26w_4s(pretrained=True)
         # ---- Receptive Field Block like module ----
         self.rfb2_1 = RFB_modified(512, channel)
         self.rfb3_1 = RFB_modified(1024, channel)
         self.rfb4_1 = RFB_modified(2048, channel)
+
         # ---- Partial Decoder ----
-        self.agg1 = aggregation(channel,n_class)
+        self.ParDec = aggregation(channel, n_class)
+
         # ---- reverse attention branch 4 ----
         self.ra4_conv1 = BasicConv2d(2048, 256, kernel_size=1)
-        self.ra4_conv2 = BasicConv2d(256, 256, kernel_size=5, padding=2)
+        self.ra4_conv2 = BasicConv2d(256+64, 256, kernel_size=5, padding=2)
         self.ra4_conv3 = BasicConv2d(256, 256, kernel_size=5, padding=2)
         self.ra4_conv4 = BasicConv2d(256, 256, kernel_size=5, padding=2)
         self.ra4_conv5 = BasicConv2d(256, n_class, kernel_size=1)
         # ---- reverse attention branch 3 ----
         self.ra3_conv1 = BasicConv2d(1024, 64, kernel_size=1)
-        self.ra3_conv2 = BasicConv2d(64, 64, kernel_size=3, padding=1)
+        self.ra3_conv2 = BasicConv2d(64+64, 64, kernel_size=3, padding=1)
         self.ra3_conv3 = BasicConv2d(64, 64, kernel_size=3, padding=1)
         self.ra3_conv4 = BasicConv2d(64, n_class, kernel_size=3, padding=1)
         # ---- reverse attention branch 2 ----
         self.ra2_conv1 = BasicConv2d(512, 64, kernel_size=1)
-        self.ra2_conv2 = BasicConv2d(64, 64, kernel_size=3, padding=1)
+        self.ra2_conv2 = BasicConv2d(64+64, 64, kernel_size=3, padding=1)
         self.ra2_conv3 = BasicConv2d(64, 64, kernel_size=3, padding=1)
         self.ra2_conv4 = BasicConv2d(64, n_class, kernel_size=3, padding=1)
 
-        
+        # ---- edge branch ----
         self.edge_conv1 = BasicConv2d(256, 64, kernel_size=1)
         self.edge_conv2 = BasicConv2d(64, 64, kernel_size=3, padding=1)
         self.edge_conv3 = BasicConv2d(64, 64, kernel_size=3, padding=1)
@@ -444,19 +445,20 @@ class MFSNet(nn.Module):
         x = self.resnet.conv1(x)
         x = self.resnet.bn1(x)
         x = self.resnet.relu(x)
-        x = self.resnet.maxpool(x)      # bs, 64, 88, 88
+
         # ---- low-level features ----
+        x = self.resnet.maxpool(x)      # bs, 64, 88, 88
         x1 = self.resnet.layer1(x)      # bs, 256, 88, 88
-        x2 = self.resnet.layer2(x1)     # bs, 512, 44, 44
+
         # ---- high-level features ----
-        #x2 = self.resnet.layer2(x1)     # bs, 512, 44, 44
+        x2 = self.resnet.layer2(x1)     # bs, 512, 44, 44
         x3 = self.resnet.layer3(x2)     # bs, 1024, 22, 22
         x4 = self.resnet.layer4(x3)     # bs, 2048, 11, 11
         x2_rfb = self.rfb2_1(x2)        # channel -> 32
         x3_rfb = self.rfb3_1(x3)        # channel -> 32
         x4_rfb = self.rfb4_1(x4)        # channel -> 32
 
-        
+        # ---- edge guidance ----
         x = self.edge_conv1(x1)
         x = self.edge_conv2(x)
         edge_guidance = self.edge_conv3(x)  # torch.Size([1, 64, 88, 88])
@@ -466,45 +468,53 @@ class MFSNet(nn.Module):
                                      mode='bilinear')
 
         # ---- global guidance ----
-        ra5_feat = self.agg1(x4_rfb, x3_rfb, x2_rfb)
-        lateral_map_5 = F.interpolate(ra5_feat, scale_factor=8, mode='bilinear')    # NOTES: Sup-1 (bs, 1, 44, 44) -> (bs, 1, 352, 352)
-
+        ra5_feat = self.ParDec(x4_rfb, x3_rfb, x2_rfb)
+        lateral_map_5 = F.interpolate(ra5_feat,
+                                      scale_factor=8,
+                                      mode='bilinear')    # NOTES: Sup-1 (bs, 1, 44, 44) -> (bs, 1, 352, 352)
 
         # ---- reverse attention branch_4 ----
         crop_4 = F.interpolate(ra5_feat, scale_factor=0.25, mode='bilinear')
-        x = -1*(torch.sigmoid(crop_4)) + 1
+        x = -1*(torch.sigmoid(crop_4)) + 1  # reverse
         x = x.expand(-1, 2048, -1, -1).mul(x4)
-        x = self.ra4_conv1(x)
+        x = torch.cat((self.ra4_conv1(x), F.interpolate(edge_guidance, scale_factor=1/8, mode='bilinear')), dim=1)
         x = F.relu(self.ra4_conv2(x))
         x = F.relu(self.ra4_conv3(x))
         x = F.relu(self.ra4_conv4(x))
         ra4_feat = self.ra4_conv5(x)
-        x = ra4_feat + crop_4
-        lateral_map_4 = F.interpolate(x, scale_factor=32, mode='bilinear')  # NOTES: Sup-2 (bs, 1, 11, 11) -> (bs, 1, 352, 352)
+        x = ra4_feat + crop_4   # element-wise addition
+        lateral_map_4 = F.interpolate(x,
+                                      scale_factor=32,
+                                      mode='bilinear')  # NOTES: Sup-2 (bs, 1, 11, 11) -> (bs, 1, 352, 352)
 
         # ---- reverse attention branch_3 ----
         crop_3 = F.interpolate(x, scale_factor=2, mode='bilinear')
         x = -1*(torch.sigmoid(crop_3)) + 1
         x = x.expand(-1, 1024, -1, -1).mul(x3)
-        x = self.ra3_conv1(x)
+        x = torch.cat((self.ra3_conv1(x), F.interpolate(edge_guidance, scale_factor=1/4, mode='bilinear')), dim=1)
         x = F.relu(self.ra3_conv2(x))
         x = F.relu(self.ra3_conv3(x))
         ra3_feat = self.ra3_conv4(x)
         x = ra3_feat + crop_3
-        lateral_map_3 = F.interpolate(x, scale_factor=16, mode='bilinear')  # NOTES: Sup-3 (bs, 1, 22, 22) -> (bs, 1, 352, 352)
+        lateral_map_3 = F.interpolate(x,
+                                      scale_factor=16,
+                                      mode='bilinear')  # NOTES: Sup-3 (bs, 1, 22, 22) -> (bs, 1, 352, 352)
 
-        # ---- reverse attention branch_2 ----
+        
         crop_2 = F.interpolate(x, scale_factor=2, mode='bilinear')
+
         x = -1*(torch.sigmoid(crop_2)) + 1
         x = x.expand(-1, 512, -1, -1).mul(x2)
-        x = self.ra2_conv1(x)
+        x = torch.cat((self.ra2_conv1(x), F.interpolate(edge_guidance, scale_factor=1/2, mode='bilinear')), dim=1)
         x = F.relu(self.ra2_conv2(x))
         x = F.relu(self.ra2_conv3(x))
         ra2_feat = self.ra2_conv4(x)
         x = ra2_feat + crop_2
-        lateral_map_2 = F.interpolate(x, scale_factor=8, mode='bilinear')   # NOTES: Sup-4 (bs, 1, 44, 44) -> (bs, 1, 352, 352)
+        lateral_map_2 = F.interpolate(x,
+                                      scale_factor=8,
+                                      mode='bilinear')   # NOTES: Sup-4 (bs, 1, 44, 44) -> (bs, 1, 352, 352)
 
-        return lateral_map_5, lateral_map_4, lateral_map_3, lateral_map_2
+        return lateral_map_5, lateral_map_4, lateral_map_3, lateral_map_2, lateral_edge
 
 def get_loader(image_root, gt_root, batchsize, trainsize, shuffle=True, num_workers=4, pin_memory=True):
 
@@ -593,7 +603,7 @@ def train(train_loader, model, optimizer, epoch):
                 images = F.upsample(images, size=(trainsize, trainsize), mode='bilinear', align_corners=True)
                 gts = F.upsample(gts, size=(trainsize, trainsize), mode='bilinear', align_corners=True)
             # ---- forward ----
-            lateral_map_5, lateral_map_4, lateral_map_3, lateral_map_2 = model(images)
+            lateral_map_5, lateral_map_4, lateral_map_3, lateral_map_2, lateral_edges = model(images)
             # ---- loss function ----
             loss5 = structure_loss(lateral_map_5, gts)
             loss4 = structure_loss(lateral_map_4, gts)
@@ -661,8 +671,7 @@ if __name__ == '__main__':
 
     print("#"*20, "Start Training", "#"*20)
 
-    # TODO mudar o numero de epocas no range
-    for epoch in range(1, 2):
+    for epoch in range(100):
         adjust_lr(optimizer, opt.lr, epoch, opt.decay_rate, opt.decay_epoch)
         train(train_loader, model, optimizer, epoch)
 
