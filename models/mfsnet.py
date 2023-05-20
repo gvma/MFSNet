b96a5c958d7e553d1de051f7ef73a395aac7bf00
Guilherme <guiganey@gmail.com>
2023-05-20 15:07:27 -0300

    Refactor

---
+++ models/mfsnet.py
@@ -0,0 +1,121 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from models.resnet import res2net50_v1b_26w_4s
+
+from rfbs.rfb_modified import RFB_modified
+from convs.basic_conv_2d import BasicConv2d
+from aggregations.aggregation import aggregation
+
+class MFSNet(nn.Module):
+    # res2net based encoder decoder
+    def __init__(self, channel=32,n_class=1):
+        super(MFSNet, self).__init__()
+        # ---- ResNet Backbone ----
+        self.resnet = res2net50_v1b_26w_4s(pretrained=False)
+        # ---- Receptive Field Block like module ----
+        self.rfb2_1 = RFB_modified(512, channel)
+        self.rfb3_1 = RFB_modified(1024, channel)
+        self.rfb4_1 = RFB_modified(2048, channel)
+
+        # ---- Partial Decoder ----
+        self.ParDec = aggregation(channel, n_class)
+
+        # ---- reverse attention branch 4 ----
+        self.ra4_conv1 = BasicConv2d(2048, 256, kernel_size=1)
+        self.ra4_conv2 = BasicConv2d(256+64, 256, kernel_size=5, padding=2)
+        self.ra4_conv3 = BasicConv2d(256, 256, kernel_size=5, padding=2)
+        self.ra4_conv4 = BasicConv2d(256, 256, kernel_size=5, padding=2)
+        self.ra4_conv5 = BasicConv2d(256, n_class, kernel_size=1)
+        # ---- reverse attention branch 3 ----
+        self.ra3_conv1 = BasicConv2d(1024, 64, kernel_size=1)
+        self.ra3_conv2 = BasicConv2d(64+64, 64, kernel_size=3, padding=1)
+        self.ra3_conv3 = BasicConv2d(64, 64, kernel_size=3, padding=1)
+        self.ra3_conv4 = BasicConv2d(64, n_class, kernel_size=3, padding=1)
+        # ---- reverse attention branch 2 ----
+        self.ra2_conv1 = BasicConv2d(512, 64, kernel_size=1)
+        self.ra2_conv2 = BasicConv2d(64+64, 64, kernel_size=3, padding=1)
+        self.ra2_conv3 = BasicConv2d(64, 64, kernel_size=3, padding=1)
+        self.ra2_conv4 = BasicConv2d(64, n_class, kernel_size=3, padding=1)
+
+        # ---- edge branch ----
+        self.edge_conv1 = BasicConv2d(256, 64, kernel_size=1)
+        self.edge_conv2 = BasicConv2d(64, 64, kernel_size=3, padding=1)
+        self.edge_conv3 = BasicConv2d(64, 64, kernel_size=3, padding=1)
+        self.edge_conv4 = BasicConv2d(64, n_class, kernel_size=3, padding=1)
+
+    def forward(self, x):
+        x = self.resnet.conv1(x)
+        x = self.resnet.bn1(x)
+        x = self.resnet.relu(x)
+
+        # ---- low-level features ----
+        x = self.resnet.maxpool(x)      # bs, 64, 88, 88
+        x1 = self.resnet.layer1(x)      # bs, 256, 88, 88
+
+        # ---- high-level features ----
+        x2 = self.resnet.layer2(x1)     # bs, 512, 44, 44
+        x3 = self.resnet.layer3(x2)     # bs, 1024, 22, 22
+        x4 = self.resnet.layer4(x3)     # bs, 2048, 11, 11
+        x2_rfb = self.rfb2_1(x2)        # channel -> 32
+        x3_rfb = self.rfb3_1(x3)        # channel -> 32
+        x4_rfb = self.rfb4_1(x4)        # channel -> 32
+
+        # ---- edge guidance ----
+        x = self.edge_conv1(x1)
+        x = self.edge_conv2(x)
+        edge_guidance = self.edge_conv3(x)  # torch.Size([1, 64, 88, 88])
+        lateral_edge = self.edge_conv4(edge_guidance)   # NOTES: Sup-2 (bs, 1, 88, 88) -> (bs, 1, 352, 352)
+        lateral_edge = F.interpolate(lateral_edge,
+                                     scale_factor=4,
+                                     mode='bilinear')
+
+        # ---- global guidance ----
+        ra5_feat = self.ParDec(x4_rfb, x3_rfb, x2_rfb)
+        lateral_map_5 = F.interpolate(ra5_feat,
+                                      scale_factor=8,
+                                      mode='bilinear')    # NOTES: Sup-1 (bs, 1, 44, 44) -> (bs, 1, 352, 352)
+
+        # ---- reverse attention branch_4 ----
+        crop_4 = F.interpolate(ra5_feat, scale_factor=0.25, mode='bilinear')
+        x = -1*(torch.sigmoid(crop_4)) + 1  # reverse
+        x = x.expand(-1, 2048, -1, -1).mul(x4)
+        x = torch.cat((self.ra4_conv1(x), F.interpolate(edge_guidance, scale_factor=1/8, mode='bilinear')), dim=1)
+        x = F.relu(self.ra4_conv2(x))
+        x = F.relu(self.ra4_conv3(x))
+        x = F.relu(self.ra4_conv4(x))
+        ra4_feat = self.ra4_conv5(x)
+        x = ra4_feat + crop_4   # element-wise addition
+        lateral_map_4 = F.interpolate(x,
+                                      scale_factor=32,
+                                      mode='bilinear')  # NOTES: Sup-2 (bs, 1, 11, 11) -> (bs, 1, 352, 352)
+
+        # ---- reverse attention branch_3 ----
+        crop_3 = F.interpolate(x, scale_factor=2, mode='bilinear')
+        x = -1*(torch.sigmoid(crop_3)) + 1
+        x = x.expand(-1, 1024, -1, -1).mul(x3)
+        x = torch.cat((self.ra3_conv1(x), F.interpolate(edge_guidance, scale_factor=1/4, mode='bilinear')), dim=1)
+        x = F.relu(self.ra3_conv2(x))
+        x = F.relu(self.ra3_conv3(x))
+        ra3_feat = self.ra3_conv4(x)
+        x = ra3_feat + crop_3
+        lateral_map_3 = F.interpolate(x,
+                                      scale_factor=16,
+                                      mode='bilinear')  # NOTES: Sup-3 (bs, 1, 22, 22) -> (bs, 1, 352, 352)
+
+
+        crop_2 = F.interpolate(x, scale_factor=2, mode='bilinear')
+
+        x = -1*(torch.sigmoid(crop_2)) + 1
+        x = x.expand(-1, 512, -1, -1).mul(x2)
+        x = torch.cat((self.ra2_conv1(x), F.interpolate(edge_guidance, scale_factor=1/2, mode='bilinear')), dim=1)
+        x = F.relu(self.ra2_conv2(x))
+        x = F.relu(self.ra2_conv3(x))
+        ra2_feat = self.ra2_conv4(x)
+        x = ra2_feat + crop_2
+        lateral_map_2 = F.interpolate(x,
+                                      scale_factor=8,
+                                      mode='bilinear')   # NOTES: Sup-4 (bs, 1, 44, 44) -> (bs, 1, 352, 352)
+
+        return lateral_map_5, lateral_map_4, lateral_map_3, lateral_map_2, lateral_edge
---
+++ models/resnet.py
@@ -0,0 +1,123 @@
+import torch
+import torch.nn as nn
+import torch.utils.model_zoo as model_zoo
+
+from bottleneck.bottle2neck import Bottle2neck
+
+model_urls = {
+    'res2net50_v1b_26w_4s': 'https://shanghuagao.oss-cn-beijing.aliyuncs.com/res2net/res2net50_v1b_26w_4s-3cf99910.pth',
+    'res2net101_v1b_26w_4s': 'https://shanghuagao.oss-cn-beijing.aliyuncs.com/res2net/res2net101_v1b_26w_4s-0812c246.pth',
+}
+
+class Res2Net(nn.Module):
+
+    def __init__(self, block, layers, baseWidth=26, scale=4, num_classes=1000):
+        self.inplanes = 64
+        super(Res2Net, self).__init__()
+        self.baseWidth = baseWidth
+        self.scale = scale
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 32, 3, 2, 1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(32, 32, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(32, 64, 3, 1, 1, bias=False)
+        )
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU()
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.avgpool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.AvgPool2d(kernel_size=stride, stride=stride,
+                             ceil_mode=True, count_include_pad=False),
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=1, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample=downsample,
+                            stype='stage', baseWidth=self.baseWidth, scale=self.scale))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes, baseWidth=self.baseWidth, scale=self.scale))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
+
+def res2net50_v1b(pretrained=False, **kwargs):
+    
+    model = Res2Net(Bottle2neck, [3, 4, 6, 3], baseWidth=26, scale=4, **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['res2net50_v1b_26w_4s']))
+    return model
+
+
+def res2net101_v1b(pretrained=False, **kwargs):
+    
+    model = Res2Net(Bottle2neck, [3, 4, 23, 3], baseWidth=26, scale=4, **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['res2net101_v1b_26w_4s']))
+    return model
+
+
+def res2net50_v1b_26w_4s(pretrained=False, **kwargs):
+    
+    model = Res2Net(Bottle2neck, [3, 4, 6, 3], baseWidth=26, scale=4, **kwargs)
+    # if pretrained:
+        # model_state = torch.load('Snapshots/Res2net/res2net50.pth')
+        # model.load_state_dict(model_state)
+        # lib.load_state_dict(model_zoo.load_url(model_urls['res2net50_v1b_26w_4s']))
+        # model.load_state_dict(model_zoo.load_url(model_urls['res2net50_v1b_26w_4s']))
+
+    return model
+
+
+def res2net101_v1b_26w_4s(pretrained=False, **kwargs):
+   
+    model = Res2Net(Bottle2neck, [3, 4, 23, 3], baseWidth=26, scale=4, **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['res2net101_v1b_26w_4s']))
+    return model
+
+
+def res2net152_v1b_26w_4s(pretrained=False, **kwargs):
+   
+    model = Res2Net(Bottle2neck, [3, 8, 36, 3], baseWidth=26, scale=4, **kwargs)
+    if pretrained:
+        model.load_state_dict(model_zoo.load_url(model_urls['res2net152_v1b_26w_4s']))
+    return model
